9d89f39a09cf1e22498071d248270f0c0510b70e
kongjialin <kongjialin92@gmail.com>
2016-05-21 20:55:34 +0800

    format the code and readme

--- src/com/kong/bop/Rest.java
+++ src/com/kong/bop/Rest.java
@@ -20,41 +20,42 @@ import org.json.JSONObject;
 @Path("/rest")
 public class Rest {
 
-	@GET
-	@Produces("application/json")
-	public static Response FindPaths(@QueryParam("id1") Long id1, @QueryParam("id2") Long id2) throws JSONException {
-		HttpClient httpclient = HttpClients.createDefault();
-		String expr = "Composite(AA.AuId=" + id1 + ")&count=10000&attributes=Id,AA.AuId,AA.AfId";
-		JSONObject json = SendRequset(httpclient, expr);
-		JSONArray entities1 = json.getJSONArray("entities");
+  @GET
+  @Produces("application/json")
+  public static Response FindPaths(@QueryParam("id1") Long id1, @QueryParam("id2") Long id2)
+      throws JSONException {
+    HttpClient httpclient = HttpClients.createDefault();
+    String expr = "Composite(AA.AuId=" + id1 + ")&count=10000&attributes=Id,AA.AuId,AA.AfId";
+    JSONObject json = SendRequset(httpclient, expr);
+    JSONArray entities1 = json.getJSONArray("entities");
 
-		expr = "Composite(AA.AuId=" + id2 + ")&count=10000&attributes=Id,AA.AuId,AA.AfId";
-		json = SendRequset(httpclient, expr);
-		JSONArray entities2 = json.getJSONArray("entities");
-		
-		List<List<Long>> paths = null;
-		if (entities1.length() == 0 && entities2.length() == 0) {
-			paths = IdId.Calculate(httpclient, id1, id2);
-		} else if (entities1.length() == 0 && entities2.length() > 0) {
-			paths = IdAuId.Calculate(httpclient, id1, id2, entities2);
-		} else if (entities1.length() > 0 && entities2.length() == 0) {
-			paths = AuIdId.Calculate(httpclient, id1, id2, entities1);
-		} else {
-			paths = AuIdAuId.Calculate(httpclient, id1, id2, entities1, entities2);
-		}
-		
-		JSONArray response = new JSONArray(paths);
-		String result = response.toString();
-		return Response.status(200).entity(result).build();
-	}
-	
-	public static JSONObject SendRequset(HttpClient httpclient, String expr) {
-		String response = MAGAPI.Get(httpclient, expr);
-//		System.out.println(response);
-		// error checking
-		// while (response == null) response = MAGAPI.Get(httpclient, expr);
-		// while(regex解析response的值为error) response = MAGAPI.Get(httpclient, expr);
-		return new JSONObject(response);
-	}
+    expr = "Composite(AA.AuId=" + id2 + ")&count=10000&attributes=Id,AA.AuId,AA.AfId";
+    json = SendRequset(httpclient, expr);
+    JSONArray entities2 = json.getJSONArray("entities");
+
+    List<List<Long>> paths = null;
+    if (entities1.length() == 0 && entities2.length() == 0) {
+      paths = IdId.Calculate(httpclient, id1, id2);
+    } else if (entities1.length() == 0 && entities2.length() > 0) {
+      paths = IdAuId.Calculate(httpclient, id1, id2, entities2);
+    } else if (entities1.length() > 0 && entities2.length() == 0) {
+      paths = AuIdId.Calculate(httpclient, id1, id2, entities1);
+    } else {
+      paths = AuIdAuId.Calculate(httpclient, id1, id2, entities1, entities2);
+    }
+
+    JSONArray response = new JSONArray(paths);
+    String result = response.toString();
+    return Response.status(200).entity(result).build();
+  }
+
+  public static JSONObject SendRequset(HttpClient httpclient, String expr) {
+    String response = MAGAPI.Get(httpclient, expr);
+    // System.out.println(response);
+    // error checking
+    // while (response == null) response = MAGAPI.Get(httpclient, expr);
+    // while(regex解析response的值为error) response = MAGAPI.Get(httpclient, expr);
+    return new JSONObject(response);
+  }
 }
 
--- src/com/kong/bop/Util.java
+++ src/com/kong/bop/Util.java
@@ -9,75 +9,75 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Util {
-	public static void ParseJson(JSONObject entity, Set<Long> RId, Set<Long> Other, Set<Long> AuIds) {
-		if (Other != null) {
-			JSONArray Fs = entity.optJSONArray("F");
-			if (Fs != null) {
-				for (int i = 0; i < Fs.length(); ++i) {
-					Other.add(Fs.getJSONObject(i).getLong("FId"));
-				}
-			}
-			JSONObject J = entity.optJSONObject("J");
-			if (J != null) {
-				Other.add(J.getLong("JId"));
-			}
-			JSONObject C = entity.optJSONObject("C");
-			if (C != null) {
-				Other.add(C.getLong("CId"));
-			}
-			JSONArray AAs = entity.optJSONArray("AA");
-			if (AAs != null) {
-				for (int i = 0; i < AAs.length(); i++) {
-					Other.add(AAs.getJSONObject(i).getLong("AuId"));
-					if (AuIds != null) AuIds.add(AAs.getJSONObject(i).getLong("AuId"));
-				}
-			}
-		}
-		
-		if (RId != null) {
-			JSONArray RIds = entity.optJSONArray("RId");
-			if (RIds != null) {
-				for (int i = 0; i < RIds.length(); ++i) {
-					RId.add(RIds.getLong(i));
-				}
-			}
-		}
-	}
-	
-	public static List<Long> Intersection(Set<Long> s1, Set<Long> s2) {
-		List<Long> common = new ArrayList<Long>();
-		Set<Long> small = s1, big = s2;
-		if (s1.size() > s2.size()) {
-			small = s2;
-			big = s1;
-		}
-		for (Long other : small) {
-			if (big.contains(other)) common.add(other);
-		}
-		return common;
-	}
+  public static void ParseJson(JSONObject entity, Set<Long> RId, Set<Long> Other, Set<Long> AuIds) {
+    if (Other != null) {
+      JSONArray Fs = entity.optJSONArray("F");
+      if (Fs != null) {
+        for (int i = 0; i < Fs.length(); ++i) {
+          Other.add(Fs.getJSONObject(i).getLong("FId"));
+        }
+      }
+      JSONObject J = entity.optJSONObject("J");
+      if (J != null) {
+        Other.add(J.getLong("JId"));
+      }
+      JSONObject C = entity.optJSONObject("C");
+      if (C != null) {
+        Other.add(C.getLong("CId"));
+      }
+      JSONArray AAs = entity.optJSONArray("AA");
+      if (AAs != null) {
+        for (int i = 0; i < AAs.length(); i++) {
+          Other.add(AAs.getJSONObject(i).getLong("AuId"));
+          if (AuIds != null) AuIds.add(AAs.getJSONObject(i).getLong("AuId"));
+        }
+      }
+    }
 
-	public static String CreateOR(List<Long> ids, int begin, int end) {
-		if (begin == end) return "Id=" + ids.get(begin);
-		int mid = (begin + end) / 2;
-		return "Or(" + CreateOR(ids, begin, mid) + "," + CreateOR(ids, mid + 1, end) + ")";
-	}
-	
-	public static Set<Long> GetAfIds(JSONArray entities, Long AuId) {
-		Set<Long> result = new HashSet<>();
-		for (int i = 0; i < entities.length(); i++) {
-			JSONArray AAs = entities.getJSONObject(i).getJSONArray("AA");
-			for (int j = 0; j < AAs.length(); j++) {
-				JSONObject aa = AAs.getJSONObject(j);
-				if (AuId.equals(aa.getLong("AuId"))) {
-					Long afid = aa.optLong("AfId");
-					if (afid != null) result.add(afid);
-					break;
-				}
-			}
-		}
-		result.remove(new Long(0));
-		return result;
-	}
+    if (RId != null) {
+      JSONArray RIds = entity.optJSONArray("RId");
+      if (RIds != null) {
+        for (int i = 0; i < RIds.length(); ++i) {
+          RId.add(RIds.getLong(i));
+        }
+      }
+    }
+  }
+
+  public static List<Long> Intersection(Set<Long> s1, Set<Long> s2) {
+    List<Long> common = new ArrayList<Long>();
+    Set<Long> small = s1, big = s2;
+    if (s1.size() > s2.size()) {
+      small = s2;
+      big = s1;
+    }
+    for (Long other : small) {
+      if (big.contains(other)) common.add(other);
+    }
+    return common;
+  }
+
+  public static String CreateOR(List<Long> ids, int begin, int end) {
+    if (begin == end) return "Id=" + ids.get(begin);
+    int mid = (begin + end) / 2;
+    return "Or(" + CreateOR(ids, begin, mid) + "," + CreateOR(ids, mid + 1, end) + ")";
+  }
+
+  public static Set<Long> GetAfIds(JSONArray entities, Long AuId) {
+    Set<Long> result = new HashSet<>();
+    for (int i = 0; i < entities.length(); i++) {
+      JSONArray AAs = entities.getJSONObject(i).getJSONArray("AA");
+      for (int j = 0; j < AAs.length(); j++) {
+        JSONObject aa = AAs.getJSONObject(j);
+        if (AuId.equals(aa.getLong("AuId"))) {
+          Long afid = aa.optLong("AfId");
+          if (afid != null) result.add(afid);
+          break;
+        }
+      }
+    }
+    result.remove(new Long(0));
+    return result;
+  }
 
 }
